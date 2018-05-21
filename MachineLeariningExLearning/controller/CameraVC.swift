d97756a2e6068571abfe8dcec26a77cb727a503b
durgaprasad <imadurgaprasad@gmail.com>
2018-05-21 18:27:45 +0530

    Initial commit
    My first commit

---
+++ MachineLeariningExLearning/controller/CameraVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MachineLeariningExLearning
+//
+//  Created by IBM-MOBILITY on 21/05/18.
+//  Copyright © 2018 IBM-MOBILITY. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
